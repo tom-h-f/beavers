ef72d2cd6bba69eeba9ade4d350e5c55d50e5b0c
Tom French <thomas.harry.french@icloud.com>
2025-10-02 08:58:56 +0100

    wip

---
+++ beavers-python/enviroment.py
@@ -0,0 +1,28 @@
+import numpy as np
+import random
+
+# double turbulence(double x, double y, double z, double f)
+# {
+#  double t = -.5;
+#  for (; f < = W / 12; f *= 2) // W = Image width in pixels
+#    t += abs(noise(x, y, z, f) / f);
+#  return t;
+# }
+
+
+class Enviroment:
+    def __init__(self, width: int = 128, height: int = 128):
+        self.width = width
+        self.height = width
+
+    def generate_world(self):
+        # TODO: This needs to become a actual, complicated world generation built for generating rivers.
+        # Also, we will need to randomly add trees, maybe in another array (this probs just has to be a seperate `terrain` class tbh)
+        random.seed()
+        ground_chance = 0.3
+        water_chance = 0.7
+        self.world_grid = np.random.choice(
+            [0, 1], size=(self.height, self.width), p=[ground_chance, water_chance]
+        )
+        print(f"Water Tile Count: {(self.world_grid == 1).sum()}")
+        print(f"Ground Tile Count: {(self.world_grid == 0).sum()}")
---
+++ beavers-python/main.py
@@ -0,0 +1,10 @@
+import enviroment
+
+
+def main():
+    env = enviroment.Enviroment()
+    env.generate_world()
+
+
+if __name__ == "__main__":
+    main()
